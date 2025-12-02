3b11e54060e63a681fe3f881143b9a6e9428f000
Mikey Kilani <mikeykilani@Mikeys-MBP.lan>
2025-12-01 22:49:49 -0500

    adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1268083376212572"
+          crossOrigin="anonymous">
+        </script>
+      </head>
+      <body className="antialiased">
+        {children}
+      </body>
     </html>
   );
 }
